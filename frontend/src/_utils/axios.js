ca31409c52a85d122817d639c25f7bfe82f38ed6
vandyke1906 <vandyke1906@gmail.com>
2025-05-26 10:38:02 +0800

    Revert "fixed bearer token"
    This reverts commit bfa32bcff243290743d758fd6cd32b2b2095f8d6.

--- frontend/src/_utils/axios.js
+++ frontend/src/_utils/axios.js
@@ -7,14 +7,14 @@ const ApiClient = axios.create({
 });
 
 // Automatically add the Bearer token to requests
-// ApiClient.interceptors.request.use((config) => {
-//   const userData = JSON.parse(sessionStorage.getItem("user")); // Retrieve user data from session storage
-//   const token = userData?.token; // Extract the token correctly
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// }, (error) => Promise.reject(error));
+ApiClient.interceptors.request.use((config) => {
+  const userData = JSON.parse(sessionStorage.getItem("user")); // Retrieve user data from session storage
+  const token = userData?.data?.token; // Extract the token correctly
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => Promise.reject(error));
 
 
 const ApiBasic = axios.create({
