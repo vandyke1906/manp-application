add1180ce840b45bf2b7e5f8a1895700e9d5f24e
vandyke1906 <vandyke1906@gmail.com>
2025-06-04 13:53:36 +0800

    added fillable for approval and comment table

---
+++ backend/app/Models/Approval.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Approval extends Model
+{
+    /** @use HasFactory<\Database\Factories\ApprovalFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'application_id',
+        'user_id',
+        'status',
+        'approved_at',
+    ];
+}
